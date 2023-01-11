cc454251e7553705905180992c6ccdf4f1992e27
INDHUMATHI2003 <101732929+INDHUMATHI2003@users.noreply.github.com>
2023-01-11 11:12:48 +0530

    python

---
+++ Goldman Sachs/Count Nice Pairs in an Array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countNicePairs(self, nums: List[int]) -> int:
+        def reverse(a):
+           b=str(a)
+           r=b[::-1]
+           return int(r)
+        freq={}
+        res=0
+        for i in nums:
+            s=i-reverse(i)
+            if s in freq.keys():
+                freq[s]+=1
+                res+=freq[s]
+            else:
+                freq[s]=0
+        return int(res%(1e9+7))
+Console
