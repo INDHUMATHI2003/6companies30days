598b33ffebb4a1834d777c9d24fa69562fd396eb
INDHUMATHI2003 <101732929+INDHUMATHI2003@users.noreply.github.com>
2023-01-13 12:45:19 +0530

    python

---
+++ Goldman Sachs/All Elements in Two Binary Search Trees.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        arr1,arr2,ans=[],[],[]
+        
+        def inorder(root,arr):
+           if not root:
+                return
+           inorder(root.left,arr)
+           arr.append(root.val)
+           inorder(root.right,arr) 
+        inorder(root1,arr1)
+        inorder(root2,arr2)  
+        i=0
+        j=0
+        while i<len(arr1) and j<len(arr2):
+            if arr1[i]<=arr2[j]:
+                ans.append(arr1[i])
+                i+=1
+            else: 
+                ans.append(arr2[j])
+                j+=1
+        while i<len(arr1):
+            ans.append(arr1[i])
+            i+=1
+        while j<len(arr2):
+            ans.append(arr2[j])
+            j+=1
+        return ans
+
